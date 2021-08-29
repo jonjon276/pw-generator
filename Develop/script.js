cfea9eeefaefa2b8cc5878f771ecac58ec4e524c
Jay the chef <webseo76@gmail.com>
2021-08-29 15:57:39 -0700

    added do while loop to password prompt

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,10 @@
 var lCaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uCaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var specialCharacters = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','/',':',';','<','=','>','?','@','[',']','^','_','`','{','}','|','~'];
-
+var containsLowerCase = false;
+var containsUpperCase = false;
+var containsSpecialChar = false;
+var containsNumeric = false;
 //random Number function
 function randNum() {
   Math.floor(Math.random());
@@ -14,13 +17,34 @@ var generateBtn = document.querySelector("#generate");
 //ask user for password criteria
 //ask for password length, this will equal how many characters should be generated. possible loop until length is equaled
 function generatePassword() {
+  //getting the password length from the user
   var passwordLength = parseInt(prompt("How long would you like your password to be? Please pick a number between 8 and 128."));
-  if (passwordLength !==NaN && passwordLength >= 8 && passwordLength <= 128) {
-  return passwordLength;
-  } else {
-    passwordLength = parseInt(prompt('Please make your selection a NUMBER between 8 and 128.'));
+  
+    if (passwordLength !==NaN && passwordLength >= 8 && passwordLength <= 128) {
+      containsLowerCase = confirm('Would you like lower case letters in your password?');
+      containsUpperCase = confirm('Would you like upper case letters in your password?');
+      containsSpecialChar = confirm('Would you like special characters in your password?');
+      containsNumeric = confirm('Would you like numbers in your password?')
+      console.log(containsLowerCase, containsUpperCase, containsSpecialChar, containsNumeric);
+    } else {
+        do {
+    passwordLength = parseInt(prompt('Try Again, Please enter a NUMBER BETWEEN 8 and 128 in numeric form.'));
+        }
+        while (passwordLength > 128 || passwordLength < 8);
+      containsLowerCase = confirm('Would you like lower case letters in your password.');
+      containsUpperCase = confirm('Would you like upper case letters in your password?');
+      containsSpecialChar = confirm('Would you like special characters in your password?');
+      containsNumeric = confirm('Would you like numbers in your password?')
   }
-}
+
+  if(!containsNumeric && !containsSpecialChar && !containsUpperCase && containsLowerCase) {
+    alert("Please select at least one character type for your password.");
+    return;
+  }
+
+      
+  console.log(containsLowerCase, containsUpperCase, containsSpecialChar, containsNumeric);
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
